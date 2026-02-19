38c7d25859fdc5927eaed1f29f2e7f779ee60f1a
YUVRAJDEVESHSRIVASTAVA <yuvrajsrivastava02@gmail.com>
2026-02-20 00:25:16 +0530

    Initial project setup

--- backend/src/main/java/com/school/SchoolManagementApplication.java
+++
@@ -1,7 +0,0 @@
-package com.school;
-
-public class SchoolManagementApplication {
-    public static void main(String[] args) {
-        System.out.println("Placeholder application");
-    }
-}
--- backend/src/main/java/com/school/config/PasswordEncoderConfig.java
+++
@@ -1,3 +0,0 @@
-package com.school.config;
-
-public class PasswordEncoderConfig {}
